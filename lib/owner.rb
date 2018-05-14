cba19857001d7f6725a4dff92c760baa007c0fc8
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-14 23:47:49 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -32,13 +32,11 @@ class Owner
   end
     
   def buy_cat(name)
-    cat = Cat.new(name)
-    self.pets[:cats] << Cat.new(name
+    self.pets[:cats] << Cat.new(name)
   end
   
   def buy_dog(name)
-    dog = Dog.new(name)
-    self.pets[:dogs] << dog
+    self.pets[:dogs] << Dog.new(name)
   end
   
   def walk_dogs
